0a9503eb6a3a195902a7fe9c1c06cc0cc18fe4ef
thebidixhd <bidiboy012@gmail.com>
2020-02-06 23:21:42 +0100

    last commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,12 +1,4 @@
 class Doctor < ApplicationRecord
-	def change
-		create_table :doctors do |t|
-			t.string :first_name
-			t.string :last_name
-			t.string :specialty
-			t.string :zip_code
-
-			t.timestamps
-		end
-	end
+	has_many :appointment
+  has_many :patient, through: :appointment
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'doctor'
+
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+13.times do
+  doctor = Doctor.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    specialty: Faker::Job.field,
+    zip_code: Faker::Address.zip_code)
+end
+
+37.times do
+  patient = Patient.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name)
+end
+
+68.times do
+  appointment = Appointment.create!(date: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short))
+end
